0b3a1b0196baa79017d2ef614528cd893aea057c
rhit-mooretj <mooretj@rose-hulman.edu>
2023-09-08 14:25:48 -0400

    Finished Follow Along 2

---
+++ Follow Along 2 - Web App Colorify/public/scripts/main.js
@@ -0,0 +1,8 @@
+function paint(color) {
+    console.log(color);
+    const circle = document.getElementById('circleID');
+    circle.style = `background-color:${color}`;
+    console.log(circle);
+}
+
+console.log('Script Loaded...')
