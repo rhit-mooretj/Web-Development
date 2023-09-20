23c8b048eb2bc29504fcfd54198a81eb9b533b03
rhit-mooretj <mooretj@rose-hulman.edu>
2023-09-20 01:02:59 -0400

    Practice 5 finished

---
+++ In-Class CSS Practice/Practice 5/public/scripts/main.js
@@ -0,0 +1,56 @@
+document.getElementById("demo").style.fontSize = "35px";
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+
+console.log(alert(5 + 6)); // This is a comment
+
+/* This is a long comment
+
+
+AHHHH
+
+*/
+
+let variable = "VARIABLE", number = 15, boolean = true;
+
+{
+    let x = 2;
+}
+
+console.log(`This value is undefined: ${x}`);
+
+// You can create a const object:
+const car = { type: "Fiat", model: "500", color: "white" };
+
+// You can change a property:
+car.color = "red";
+
+// You can add a property:
+car.owner = "Johnson";
+
+let x = 5;
+let y = 2;
+let z = x + y;
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let xBool = true;
+let yBool = false;
+
+// Object:
+const person = { firstName: "John", lastName: "Doe" };
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
